159cc18a7ee6d19ded84a41a0b69ab514d6d6a0c
William Riley <wrileystl@gmail.com>
2018-08-12 15:24:58 -0500

    ensure only one instance is running

--- FarmSimBackupManager/Program.cs
+++ FarmSimBackupManager/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -11,9 +12,24 @@ namespace FarmSimBackupManager
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        
+        private static Mutex mutex = null;
+
         [STAThread]
         static void Main()
         {
+            const string appName = "FarmSimBackupManager";
+            bool createdNew;
+
+            mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                //app is already running! Exiting the application
+                MessageBox.Show(appName + " is already running!");
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
