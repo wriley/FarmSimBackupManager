b5d5b6369111a7e41d4a21481860efe393e7ae53
William Riley <wrileystl@gmail.com>
2018-11-20 14:51:40 -0600

    added new release and script

---
+++ release.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -f download/FarmSimBackupManager.zip
+zip -j download/FarmSimBackupManager.zip FarmSimBackupManager/bin/Release/FarmSimBackupManager.exe FarmSimBackupManager/bin/Release/ICSharpCode.SharpZipLib.dll
